e3acd8b4224e344de46ef18e71127a05f074b78d
Jouriwz <jouri.zevenhek@outlook.com>
2024-06-08 21:55:32 +0200

    refactoring create and show listing to controller

--- App/controllers/HomeController.php
+++ App/controllers/HomeController.php
@@ -2,13 +2,24 @@
 
 namespace App\Controllers;
 
-class HomeController {
+use Framework\Database;
+
+class HomeController
+{
+    protected $db;
     public function __construct ()
     {
-
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
     }
 
-    public function index() {
-        die('HomeController@index');
+    public function index()
+    {
+        $listings = $this->db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+
+        loadView('home', [
+            'listings' => $listings
+        ]);
+
     }
 }
